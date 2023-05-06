c6b78430c20777a2d3d2f4e1c23ad2ab7ddb0c6e
LucasDev <LucasDev9645@gmail.com>
2023-05-06 17:12:16 -0300

    Creating UserDataCard

--- src/components/FindProfile/index.tsx
+++ src/components/FindProfile/index.tsx
@@ -1,15 +1,19 @@
 import Button from "../Button";
+import UserDataCard from "./UserDataCard";
 import "./styles.css";
 
 const FindProfile = () => {
   return (
-    <div className="container find-Profile-container">
-      <h1>Encontre um perfil Github</h1>
-      <form>
-        <input type="text" />
-        <Button name="Encontrar" />
-      </form>
-    </div>
+    <>
+      <div className="container find-Profile-container">
+        <h1>Encontre um perfil Github</h1>
+        <form>
+          <input type="text" />
+          <Button name="Encontrar" />
+        </form>
+      </div>
+      <UserDataCard />
+    </>
   );
 };
 
